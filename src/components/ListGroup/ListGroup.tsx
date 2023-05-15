5801455185f45cf32f55e4564d76598022c662ad
Abhishek <eduandabhi@gmail.com>
2023-05-15 21:53:23 +0530

    css modules

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./ListGroup.css";
+import styles from "./ListGroup.module.css";
 interface Props {
   items: string[];
   heading: string;
@@ -12,7 +12,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
   return (
     <>
       <h1>{heading}</h1>
-      <ul className="list-group">
+      <ul className={[styles.listGroup, styles.container].join(" ")}>
         {items.length === 0 && <p>No Item Found</p>}
         {items.map((item, index) => (
           <li
