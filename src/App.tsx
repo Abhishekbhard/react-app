6cdbcf81766a46468282417bc95e6a183b9c3407
Abhishek <eduandabhi@gmail.com>
2023-05-16 14:09:08 +0530

    Updating object values

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "'Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
   };
   return (
     <div>
-      <div>{pizza.toppings}</div>
+      <div>{cart.items.map((item) => item.quantity)}</div>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
