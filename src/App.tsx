d7ab72482a64da7c5c6090953242a4daccb998f5
Abhishek <eduandabhi@gmail.com>
2023-05-16 13:57:07 +0530

    Adding item to an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
   });
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Abhi" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "'Cheese"] });
   };
   return (
     <div>
-      <div>{game.player.name}</div>
+      <div>{pizza.toppings}</div>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
