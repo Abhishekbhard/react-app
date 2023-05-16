a02ee4bf16b86cc6353a7f29abb1f423b8b29ee5
Abhishek <eduandabhi@gmail.com>
2023-05-16 15:01:04 +0530

    Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
-  };
   return (
     <div>
-      <div>{cart.items.map((item) => item.quantity)}</div>
-      <button onClick={handleClick}>Click Me</button>
+      <Form />
     </div>
   );
 }
