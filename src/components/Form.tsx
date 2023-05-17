09c15f1d4e7990bc876c95d9ab7d6476cc11717b
Abhishek <eduandabhi@gmail.com>
2023-05-17 23:50:20 +0530

    Schema based validation with zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import { useForm, FieldValues } from "react-hook-form";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const Schema = z.object({
+  name: z.string().min(3, { message: "Name must be atleast 3 characters" }),
+  age: z.number({ invalid_type_error: "Age Field is required" }).min(18),
+});
+
+type FormData = z.infer<typeof Schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(Schema) });
   const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -19,28 +24,24 @@ const Form = () => {
           name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is reuired</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be atlest 3 characters</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
